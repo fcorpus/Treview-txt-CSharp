c3ce3a40122223cb536a6c7ffd9f6b2b9f23fded
fcorpus <fcastillocorpus@gmail.com>
2023-09-13 13:23:49 -0600

    version con errores

---
+++ ArbolCastilloCorpus-CastilloPardo/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace ArbolCastilloCorpus_CastilloPardo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.arbolito = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(71, 46);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrir.TabIndex = 0;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // arbolito
+            // 
+            this.arbolito.Location = new System.Drawing.Point(236, 12);
+            this.arbolito.Name = "arbolito";
+            this.arbolito.Size = new System.Drawing.Size(490, 368);
+            this.arbolito.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.arbolito);
+            this.Controls.Add(this.btnAbrir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.TreeView arbolito;
+    }
+}
+
---
+++ ArbolCastilloCorpus-CastilloPardo/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ArbolCastilloCorpus_CastilloPardo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            string[] lines;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "Archivos de texto|*.txt";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                lines = File.ReadAllLines(openFileDialog.FileName);
+                arbolito.Nodes.Clear();
+                TreeNode fatherNode = null;
+                int previousLevel = -1;
+                foreach (string line in lines)
+                {
+                    int level = 0; ;
+
+                    while (line[level] == '.')
+                        level++;
+
+                    string textNode = line.TrimStart('.');
+                    TreeNode newNode = new TreeNode(textNode);
+
+                    if (level == previousLevel)
+                    {
+                        if (fatherNode != null)
+                        {
+                            fatherNode.Parent.Nodes.Add(newNode);
+                        }
+                    }
+                    else if (level > previousLevel)
+                    {
+                        if (fatherNode != null)
+                        {
+                            fatherNode.Nodes.Add(newNode);
+                        }
+                    }
+                    else
+                    {
+                        for (int i = previousLevel; i >= level; i--)
+                            fatherNode = fatherNode.Parent;
+                        fatherNode.Parent.Nodes.Add(newNode);
+                    }
+                    fatherNode = newNode;
+                    previousLevel = level;
+                }
+            }
+
+        }*/
+
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            string[] lines;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();   
+
+            openFileDialog.Filter = "Archivos de texto|*.txt";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                lines = File.ReadAllLines(openFileDialog.FileName);
+                arbolito.Nodes.Clear();
+                foreach (string line in lines)
+                {
+                    TreeNode fatherNode = null;
+                    string[] nodes = line.Split('.');
+                    /*if (nodes[0] != "")
+                        fatherNode = null;
+                    else
+                        fatherNode = new TreeNode(nodes[0].Trim());*/
+                    foreach (string textNode in nodes)
+                    {
+                        TreeNode newNode = new TreeNode(textNode.Trim());
+                        if (fatherNode == null)
+                        {
+                             arbolito.Nodes.Add(newNode);
+                             fatherNode = newNode;
+                        }
+                        else
+                        {
+                             fatherNode.Nodes.Add(newNode);
+                             fatherNode = newNode;
+                        }
+                    }
+                }
+            }
+
+        }
+
+
+    }
+}
+
